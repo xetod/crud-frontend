ee1501ca0d590da12340da076d3a2f0edc9762eb
javad.memariani <javad.memariani@gmail.com>
2023-07-14 11:44:45 +0330

    add error boundary

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -3,6 +3,7 @@ import Main from "../Main/Main";
 import AppStateProvider from "../../context/AppStateProvider";
 import NewCustomer from "../../components/Customer/NewCustomer/NewCustomer";
 import UpdateCustomer from "../../components/Customer/UpdateCustomer/UpdateCustomer";
+import ErrorBoundary from "../../errorboundary/ErrorBoundary";
 
 function App() {
   return (
@@ -11,20 +12,21 @@ function App() {
       <div className="p-2">
         <h1>Crudio!</h1>
       </div>
-      
-      {/* Provide the application state */}
-      <AppStateProvider>
-        {/* Set up the browser router */}
-        <BrowserRouter>
-          {/* Define the routes */}
-          <Routes>
-            {/* Define a route for the main component */}
-            <Route exact path="/" element={<Main />} />
-            <Route exact path="/new-customer" element={<NewCustomer />} />
-            <Route exact path="/update-customer" element={<UpdateCustomer />} />
-          </Routes>
-        </BrowserRouter>
-      </AppStateProvider>
+      <ErrorBoundary>
+        {/* Provide the application state */}
+        <AppStateProvider>
+          {/* Set up the browser router */}
+          <BrowserRouter>
+            {/* Define the routes */}
+            <Routes>
+              {/* Define a route for the main component */}
+              <Route exact path="/" element={<Main />} />
+              <Route exact path="/new-customer" element={<NewCustomer />} />
+              <Route exact path="/update-customer" element={<UpdateCustomer />} />
+            </Routes>
+          </BrowserRouter>
+        </AppStateProvider>
+      </ErrorBoundary>
     </div>
   );
 }
